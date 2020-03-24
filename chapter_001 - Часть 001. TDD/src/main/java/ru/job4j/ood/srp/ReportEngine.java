eea34223d068fc1f0a0b8c117654dec486882f4a
DenisViskov <denisviskov16@gmail.com>
2020-03-24 19:46:31 +0300

    1. Отчеты.[#259133]

--- chapter_001 - Часть 001. TDD/src/main/java/ru/job4j/ood/srp/ReportEngine.java
+++ chapter_001 - Часть 001. TDD/src/main/java/ru/job4j/ood/srp/ReportEngine.java
@@ -27,12 +27,12 @@ public class ReportEngine {
      */
     public String generate(Predicate<Employer> filter) {
         StringBuilder text = new StringBuilder();
-        text.append("Name; Hired; Fired; Salary");
+        text.append("Name; Hired; Fired; Salary;" + System.lineSeparator());
         for (Employer employer : store.findBy(filter)) {
             text.append(employer.getName()).append(";")
                     .append(employer.getHired()).append(";")
                     .append(employer.getFired()).append(";")
-                    .append(employer.getSalary()).append(";");
+                    .append(employer.getSalary()).append(";" + System.lineSeparator());
         }
         return text.toString();
     }
--- chapter_001 - Часть 001. TDD/src/test/java/ru/job4j/ood/srp/ReportEngineTest.java
+++ chapter_001 - Часть 001. TDD/src/test/java/ru/job4j/ood/srp/ReportEngineTest.java
@@ -2,6 +2,8 @@ package ru.job4j.ood.srp;
 
 import org.junit.Test;
 
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
 import java.util.Calendar;
 
 import static org.junit.Assert.*;
@@ -27,18 +29,49 @@ public class ReportEngineTest {
         assertThat(engine.generate(em -> true), is(expect.toString()));
     }
 
-    @Test
+    /*@Test
     public void whenWeCreateReportForProgrammers() {
-
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employer worker = new Employer("Ivan", now, now, 100);
+        store.add(worker);
+        ReportForProgrammers engine = new ReportForProgrammers(store);
+        HTMLDocument expected = new HTMLDocument(Path);
+        HTMLDocument out = engine.generate(i -> true);
+        assertThat(out.toString(),is(expected.toString()));
     }
 
     @Test
     public void whenWeCreateReportForAccountant() {
-
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employer worker = new Employer("Ivan", now, now, 100);
+        store.add(worker);
+        ReportEngineForAccountant engine = new ReportEngine(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(";")
+                .append(worker.getHired()).append(";")
+                .append(worker.getFired()).append(";")
+                .append(worker.getSalary()).append(";")
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true), is(expect.toString()));
     }
 
     @Test
     public void whenWeCreateReportForHR() {
-
-    }
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employer worker = new Employer("Ivan", now, now, 100);
+        store.add(worker);
+        ReportEngineForHR engine = new ReportEngine(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(";")
+                .append(worker.getSalary()).append(";")
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }*/
 }
