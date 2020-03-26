67c3671023de88f064523f6eb9a6b1a7776b848b
DenisViskov <denisviskov16@gmail.com>
2020-03-26 13:39:52 +0300

    3. Шаблонизатор.[#259126]

---
+++ chapter_001 - Часть 001. TDD/src/main/java/ru/job4j/ood/tdd/Generate.java
@@ -0,0 +1,25 @@
+package ru.job4j.ood.tdd;
+
+import java.util.Map;
+
+/**
+ * Class has realizes generating by template
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 26.03.2020
+ */
+public class Generate implements Generator {
+
+    /**
+     * Method has realizes generating string by given template in parameters
+     *
+     * @param template - template
+     * @param args     - map
+     * @return - string
+     */
+    @Override
+    public String produce(String template, Map<String, Object> args) {
+        return null;
+    }
+}
--- chapter_001 - Часть 001. TDD/src/test/java/ru/job4j/ood/tdd/GeneratorTest.java
+++ chapter_001 - Часть 001. TDD/src/test/java/ru/job4j/ood/tdd/GeneratorTest.java
@@ -11,18 +11,18 @@ import static org.hamcrest.core.Is.is;
 @Ignore
 public class GeneratorTest {
 
-    /*@Test
+    @Ignore
     public void produceTest() {
         String template = "I am a ${name}, Who are ${subject}? ";
         Map<String, Object> keys = new HashMap<>();
         keys.put("name", "Alex");
         keys.put("subject", "you");
         String expected = "I am a Petr Arsentev, Who are you? ";
-        String out = new Generator().produce(template, keys);
+        String out = new Generate().produce(template, keys);
         assertThat(out, is(expected));
     }
 
-    @Test
+    @Ignore
     public void whenWeHaveKeysWhoHasNotInTemplate() {
         String template = "I am a ${lastName}, Who are ${subject}? ";
         Map<String, Object> keys = new HashMap<>();
@@ -30,7 +30,7 @@ public class GeneratorTest {
         keys.put("subject", "you");
         boolean result = false;
         try {
-            String out = new Generator().produce(template, keys);
+            String out = new Generate().produce(template, keys);
         } catch (Exception e) {
             result = true;
         } finally {
@@ -38,7 +38,7 @@ public class GeneratorTest {
         }
     }
 
-    @Test
+    @Ignore
     public void whenWeHaveMoreKeys() {
         String template = "I am a ${name}, Who are ${subject}? ";
         Map<String, Object> keys = new HashMap<>();
@@ -47,11 +47,11 @@ public class GeneratorTest {
         keys.put("lastName", "Petrov");
         boolean result = false;
         try {
-            String out = new Generator().produce(template, keys);
+            String out = new Generate().produce(template, keys);
         } catch (Exception e) {
             result = true;
         } finally {
             assertThat(result, is(true));
         }
-    }*/
+    }
 }
