d7923ff4fb6156c86b5141bcbce445417ca95d8b
DenisViskov <denisviskov16@gmail.com>
2020-03-29 22:50:23 +0300

    3. Шаблонизатор.[#259126] updated tests

--- chapter_001 - Часть 001. TDD/src/test/java/ru/job4j/ood/tdd/GeneratorTest.java
+++ chapter_001 - Часть 001. TDD/src/test/java/ru/job4j/ood/tdd/GeneratorTest.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
+
 @Ignore
 public class GeneratorTest {
 
@@ -23,22 +24,18 @@ public class GeneratorTest {
     }
 
     @Ignore
+    @Test(expected = Exception.class)
     public void whenWeHaveKeysWhoHasNotInTemplate() {
         String template = "I am a ${lastName}, Who are ${subject}? ";
         Map<String, Object> keys = new HashMap<>();
         keys.put("name", "Alex");
         keys.put("subject", "you");
         boolean result = false;
-        try {
-            String out = new Generate().produce(template, keys);
-        } catch (Exception e) {
-            result = true;
-        } finally {
-            assertThat(result, is(true));
-        }
+        String out = new Generate().produce(template, keys);
     }
 
     @Ignore
+    @Test(expected = Exception.class)
     public void whenWeHaveMoreKeys() {
         String template = "I am a ${name}, Who are ${subject}? ";
         Map<String, Object> keys = new HashMap<>();
@@ -46,12 +43,6 @@ public class GeneratorTest {
         keys.put("subject", "you");
         keys.put("lastName", "Petrov");
         boolean result = false;
-        try {
-            String out = new Generate().produce(template, keys);
-        } catch (Exception e) {
-            result = true;
-        } finally {
-            assertThat(result, is(true));
-        }
+        String out = new Generate().produce(template, keys);
     }
 }
