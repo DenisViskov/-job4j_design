7b55ff29c79a530988a9fa760311e7c1e33a844d
DenisViskov <denisviskov16@gmail.com>
2020-04-01 20:32:13 +0300

    4.0. File[#259109]

---
+++ chapter_003 - Часть 003. Система ввода вывода/src/main/java/io/Dir.java
@@ -0,0 +1,54 @@
+package io;
+
+import java.io.File;
+
+/**
+ * Class has realizes work with files directory
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 01.04.2020
+ */
+public class Dir {
+
+    /**
+     * Method has realizes print on console file name and size that file
+     *
+     * @param path - path file
+     */
+    public void printFileNameAndSize(File path) {
+        validation(path);
+        for (File subfile : path.listFiles()) {
+            System.out.println(String.format("name : %s; size : %s", subfile.getName(), subfile.getTotalSpace()));
+        }
+    }
+
+    /**
+     * Method has realizes print on console list of files by given path in parameters
+     *
+     * @param path - File path
+     */
+    public void printDirectory(File path) {
+        validation(path);
+        System.out.println(String.format("size : %s", path.getTotalSpace()));
+        for (File subfile : path.listFiles()) {
+            System.out.println(subfile.getAbsoluteFile());
+        }
+    }
+
+    /**
+     * Method has realizes of validations by given file
+     *
+     * @param path - file
+     * @throws IllegalStateException
+     */
+    private void validation(File path) {
+        if (!path.exists()) {
+            throw new IllegalStateException(String.format("Not exist %s", path.getAbsoluteFile()));
+        }
+        if (!path.isDirectory()) {
+            throw new IllegalStateException(String.format("Not directory %s", path.getAbsoluteFile()));
+        }
+    }
+}
+
---
+++ chapter_003 - Часть 003. Система ввода вывода/src/test/java/io/DirTest.java
@@ -0,0 +1,70 @@
+package io;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class DirTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream byteOut = new PrintStream(out);
+    private final PrintStream console = System.out;
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(byteOut);
+    }
+
+    @Test
+    public void printFileNameAndSizeTest() throws IOException {
+        folder.newFile();
+        File source = folder.getRoot();
+        new Dir().printFileNameAndSize(source);
+        String expected = String.format("name : %s; size : %s",
+                source.listFiles()[0].getName(), source.listFiles()[0].getTotalSpace()
+                        + System.lineSeparator());
+        assertThat(expected.getBytes(), is(out.toByteArray()));
+    }
+
+    @Test
+    public void printDirectoryTest() throws IOException {
+        folder.newFile();
+        folder.newFile();
+        File source = folder.getRoot();
+        new Dir().printDirectory(source);
+        String expected = String.format("size : %s",
+                source.getTotalSpace() + System.lineSeparator()
+                        + source.listFiles()[0].getAbsoluteFile() + System.lineSeparator()
+                        + source.listFiles()[1].getAbsoluteFile() + System.lineSeparator());
+        assertThat(expected.getBytes(), is(out.toByteArray()));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void exceptionTest() throws IOException {
+        folder.newFile();
+        folder.newFile();
+        File source = folder.getRoot();
+        folder.delete();
+        new Dir().printDirectory(source);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(console);
+    }
+}
