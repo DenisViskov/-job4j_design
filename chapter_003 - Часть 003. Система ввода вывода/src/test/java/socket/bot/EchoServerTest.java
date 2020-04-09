57934b90e19b07fab1a8c2d0d7cae8ef2276cb19
DenisViskov <denisviskov16@gmail.com>
2020-04-09 22:03:55 +0300

    1. Бот[#229130]

---
+++ chapter_003 - Часть 003. Система ввода вывода/src/test/java/socket/bot/EchoServerTest.java
@@ -0,0 +1,56 @@
+package socket.bot;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.*;
+
+public class EchoServerTest {
+
+    @Test
+    public void whenClientSendingHelloTest() throws IOException {
+        EchoServer server = new EchoServer();
+        ByteArrayInputStream inSocket = new ByteArrayInputStream("Hello".getBytes());
+        ByteArrayOutputStream outSocket = new ByteArrayOutputStream();
+        Socket socket = mock(Socket.class);
+        when(socket.getInputStream()).thenReturn(inSocket);
+        when(socket.getOutputStream()).thenReturn(outSocket);
+        boolean out = server.controller(socket);
+        String expected = "HTTP/1.1 200 OK" + System.lineSeparator() + System.lineSeparator() + "Hello";
+        assertThat(out, is(true));
+        assertThat(expected.getBytes(), is(outSocket.toByteArray()));
+    }
+
+    @Test
+    public void whenClientSendingSomethingTest() throws IOException {
+        EchoServer server = new EchoServer();
+        ByteArrayInputStream inSocket = new ByteArrayInputStream("I send to you something bla bla bla...".getBytes());
+        ByteArrayOutputStream outSocket = new ByteArrayOutputStream();
+        Socket socket = mock(Socket.class);
+        when(socket.getInputStream()).thenReturn(inSocket);
+        when(socket.getOutputStream()).thenReturn(outSocket);
+        boolean out = server.controller(socket);
+        String expected = "HTTP/1.1 200 OK" + System.lineSeparator() + System.lineSeparator()
+                + "I send to you something bla bla bla...";
+        assertThat(out, is(true));
+        assertThat(expected.getBytes(), is(outSocket.toByteArray()));
+    }
+
+    @Test
+    public void whenClientSendingExitTest() throws IOException {
+        EchoServer server = new EchoServer();
+        ByteArrayInputStream inSocket = new ByteArrayInputStream("Exit".getBytes());
+        Socket socket = mock(Socket.class);
+        when(socket.getInputStream()).thenReturn(inSocket);
+        boolean out = server.controller(socket);
+        assertThat(out, is(false));
+    }
+}
