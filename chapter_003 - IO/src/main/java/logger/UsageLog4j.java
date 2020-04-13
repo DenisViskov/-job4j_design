285a0956c091be1b432012566c55319c370cf9be
DenisViskov <denisviskov16@gmail.com>
2020-04-13 19:24:40 +0300

    Simple Loggin Facade 4 Java.

--- chapter_003 - IO/src/main/java/logger/UsageLog4j.java
+++ chapter_003 - IO/src/main/java/logger/UsageLog4j.java
@@ -1,17 +1,12 @@
 package logger;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * @author Денис Висков
- * @version 1.0
- * @since 11.04.2020
- */
 public class UsageLog4j {
 
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
