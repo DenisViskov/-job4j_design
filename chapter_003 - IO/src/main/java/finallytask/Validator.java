11a2c7ce10c67a5c6600b42eb06e35a86014b1d8
DenisViskov <denisviskov16@gmail.com>
2020-04-15 21:07:58 +0300

    update classes finally task

--- chapter_003 - IO/src/main/java/finallytask/Validator.java
+++ chapter_003 - IO/src/main/java/finallytask/Validator.java
@@ -1,5 +1,6 @@
 package finallytask;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -17,11 +18,29 @@ public class Validator implements Validate {
             throw new IllegalArgumentException("Please restart application and enter command which contains:" + System.lineSeparator()
                     + "-d directory" + System.lineSeparator()
                     + "-n file name" + System.lineSeparator()
-                    + "-m mask,whole name or regular verb" + System.lineSeparator()
+                    + "-m mask, -f whole name or -r regular verb" + System.lineSeparator()
                     + "-o log directory"
             );
         }
         String[] split = line.split(" ");
         return true;
+    }
+
+    private Map<String, String> buildCommand(String[] spliteLine) {
+        Map<String, String> result = new HashMap<>();
+        for (int i = 0; i < spliteLine.length; i++) {
+            if (spliteLine[i].equals("-d")) {
+                result.put(spliteLine[i], spliteLine[i + 1]);
+            } else if (spliteLine[i].equals("-n")) {
+                result.put(spliteLine[i], spliteLine[i + 1]);
+            } else if (spliteLine[i].equals("-m")
+                    || spliteLine[i].equals("-f")
+                    || spliteLine[i].equals("-r")) {
+                result.put("key", spliteLine[i]);
+            } else if (spliteLine[i].equals("-o")) {
+                result.put(spliteLine[i], spliteLine[i + 1]);
+            }
+        }
+        return result;
     }
 }
