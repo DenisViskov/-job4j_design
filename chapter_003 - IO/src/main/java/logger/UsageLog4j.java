deb5cbbb8edd67baff21f7bfd09a0065b12dc7e9
DenisViskov <denisviskov16@gmail.com>
2020-04-15 19:29:43 +0300

    3. Slf4j - вывод переменных.

--- chapter_003 - IO/src/main/java/logger/UsageLog4j.java
+++ chapter_003 - IO/src/main/java/logger/UsageLog4j.java
@@ -4,15 +4,26 @@ package logger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Class is an example use log4J
+ */
 public class UsageLog4j {
 
+    /**
+     * Logger
+     */
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Denis";
+        int age = 27;
+        double weight = 70.5;
+        short iq = 150;
+        char sex = 'm';
+        byte footSize = 42;
+        long money = 10000000L;
+        boolean isProgger = true;
+        LOG.debug("Name is {}, age: {}, weight : {}, iq: {}, sex: {}, foot size: {}, money : {}, isProgger: {}",
+                name, age, weight, iq, sex, footSize, money, isProgger);
     }
 }
