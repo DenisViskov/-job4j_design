6d8666203e1ba16cb13de193fc941a8d118cb8c1
DenisViskov <DenisViskov16@gmail.com>
2020-04-28 17:49:27 +0300

    SQL

--- chapter_004 - Data Base/src/db.sql
+++ chapter_004 - Data Base/src/db.sql
@@ -3,51 +3,59 @@ create table users(
     id serial primary key,
     login varchar(20),
     whole_name varchar(50),
-    role integer references roles(id),
-    task integer references tasks(id),
-    registration_date date
+    role integer,
+    task integer,
+    registration_date date,
+    foreign key (role) references roles(id),
+    foreign key (task) references tasks(id)
 );
 
 create table roles(
     id serial primary key,
     type varchar(20),
-    accesses integer references accesses(id)
-)
+    access integer,
+    foreign key (access) references accesses(id)
+);
 
 create table accesses(
     id serial primary key,
     type varchar(20)
-)
+);
 
 create table tasks(
     id serial primary key,
-    description integer references comments(id),
-    state integer references state(id),
+    description integer,
+    state integer,
     create_date timestamp,
-    category integer references categories(id)
-)
+    category integer,
+    foreign key (description) references comments(id),
+    foreign key (state) references states(id),
+    foreign key (category) references categories(id)
+);
 
 create table categories(
     id serial primary key,
     type varchar(15)
-)
+);
 
-create table state(
+create table states(
     id serial primary key,
     type varchar(15)
-)
+);
 
 create table extensions(
     id serial primary key,
     format varchar(3),
     size numeric(4,2),
     URL text
-)
+);
 
 create table comments(
     id serial primary key,
     message text,
-    author integer references users(id),
+    author integer,
     created_time timestamp,
-    extensions integer references extensions(id)
-)
+    extension integer,
+    foreign key (author) references users(id),
+    foreign key (extension) references extensions(id)
+);
