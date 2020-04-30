428753a83e04c161d7186d162f258ecfaf045754
DenisViskov <denisviskov16@gmail.com>
2020-04-30 20:07:07 +0300

    sql

--- chapter_004 - Data Base/src/main/java/filters/select.sql
+++ chapter_004 - Data Base/src/main/java/filters/select.sql
@@ -5,3 +5,4 @@ select * from product where expired_date between '01/05/20' and '31/05/20';
 select max(price) from product;
 select count(*) from product where type_id = '...';
 select * from product where type_id in('сыр','молоко');
+select * from product join type on product.type_id = type.id as total ;
