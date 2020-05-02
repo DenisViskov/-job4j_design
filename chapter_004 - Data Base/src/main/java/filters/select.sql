1281e3fbad103e8a8904b3bf3d7304d0df95e9f5
DenisViskov <DenisViskov16@gmail.com>
2020-05-02 16:33:36 +0300

    SQL

--- chapter_004 - Data Base/src/main/java/filters/select.sql
+++ chapter_004 - Data Base/src/main/java/filters/select.sql
@@ -1,8 +1,14 @@
 
-select * from product where type_id='сыр';
+select * from (product join type on product.type_id = type.ID) as total where total.type in('Сыр');
+
 select * from product where name like 'Мороженное%';
+
 select * from product where expired_date between '01/05/20' and '31/05/20';
+
 select max(price) from product;
+
 select count(*) from product where type_id = '...';
-select * from product where type_id in('сыр','молоко');
-select * from product join type on product.type_id = type.id as total ;
+
+select * from (product join type on product.type_id = type.ID) as total where total.type in('Молочка','Сыр');
+
+
