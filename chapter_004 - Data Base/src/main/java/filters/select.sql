080f2ab3c60cfac7e27027c029ce029453bdf198
DenisViskov <denisviskov16@gmail.com>
2020-05-04 12:34:19 +0300

    Фильтры[#259056]

--- chapter_004 - Data Base/src/main/java/filters/select.sql
+++ chapter_004 - Data Base/src/main/java/filters/select.sql
@@ -11,4 +11,6 @@ select count(*) from product where type_id = '...';
 
 select * from (product as p join type as t on p.type_id = t.ID) where t.name in('Молочка','Сыр');
 
+select tp.name, count(tp.name) as typecount from (product as pr join type as tp on pr.type_id=tp.id) group by tp.name having count(*)<10;
+
 select total.name, total.type from (product join type on product.type_id = type.ID) as total;
