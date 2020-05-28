49c352680bb5c595a80445cb6d7aad47dea3c479
DenisViskov <denisviskov16@gmail.com>
2020-05-28 18:43:00 +0300

    update

---
+++ chapter_004 - Data Base/src/main/java/sqlselectbycompany/create.sql
@@ -0,0 +1,27 @@
+create TABLE company(
+id integer NOT NULL,
+name character varying,
+CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+
+create TABLE person(
+id integer NOT NULL,
+name character varying,
+company_id integer references company(id),
+CONSTRAINT person_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE company
+(
+id integer NOT NULL,
+name character varying,
+CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE person
+(
+id integer NOT NULL,
+name character varying,
+company_id integer,
+CONSTRAINT person_pkey PRIMARY KEY (id)
+);
---
+++ chapter_004 - Data Base/src/main/java/sqlselectbycompany/insert.sql
@@ -0,0 +1,20 @@
+insert into company(id,name)
+values
+(1,'first corporation'),
+(2,'second corporation'),
+(3,'third corporation'),
+(4,'four corporation'),
+(5,'five corporation');
+
+insert into person(id,name,company_id)
+values
+(1,'Ivan',2),
+(2,'Denis',5),
+(3,'Sasha',4),
+(4,'Petr',5),
+(5,'Grisha',1),
+(6,'Senya',3),
+(7,'Volodya',5),
+(8,'Alexey',3),
+(9,'Gena',1),
+(10,'Marina',3);
