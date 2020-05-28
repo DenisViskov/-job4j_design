d8630428d0073ae57f4a8ac2dc73cc28589fa76b
DenisViskov <denisviskov16@gmail.com>
2020-05-28 18:26:11 +0300

    update

--- chapter_004 - Data Base/src/main/java/sqlselectbycompany/select.sql
+++ chapter_004 - Data Base/src/main/java/sqlselectbycompany/select.sql
@@ -3,6 +3,13 @@ from
 (person as p join company as c on p.company_id=c.id)
 where p.company_id != 5;
 
+select c.name,count(c.id) as CountOfHumans
+from
+(person as p join company as c on p.company_id=c.id)
+group by(c.name)
+order by CountOfHumans desc
+limit 1;
+
 select *
 from
 (person as p join company as c on p.company_id=c.id);
