6a69a0188071dbe14dbe19bfd79a0eaa305ce36e
DenisViskov <denisviskov16@gmail.com>
2020-06-02 19:10:15 +0300

    update

--- chapter_001 - TDD/src/main/java/ru/job4j/ood/lsp/storageofgoods/ControlQuality.java
+++ chapter_001 - TDD/src/main/java/ru/job4j/ood/lsp/storageofgoods/ControlQuality.java
@@ -1,5 +1,9 @@
 package ru.job4j.ood.lsp.storageofgoods;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+
 /**
  * @author Денис Висков
  * @version 1.0
@@ -9,6 +13,29 @@ public class ControlQuality implements Control<Food, Storage> {
 
     @Override
     public Storage executeDistribution(Food good) {
+        sendToWareHouse(good.getCreateDate(), good.getExpireDate());
         return null;
     }
+
+    private Storage checkExpire(Food good) {
+        return null;
+    }
+
+    private boolean sendToWareHouse(LocalDateTime created, LocalDateTime expired) {
+        boolean result = false;
+        int hundredPercent = Period
+                .between(expired.toLocalDate(),
+                        created.toLocalDate())
+                .getDays();
+        int daysOFPassed = Period
+                .between(LocalDate.now(),
+                        created.toLocalDate())
+                .getDays();
+        int maxCountDays = (hundredPercent / 100) * 25;
+        if (daysOFPassed < maxCountDays) {
+            result = true;
+        }
+        return result;
+    }
+
 }
