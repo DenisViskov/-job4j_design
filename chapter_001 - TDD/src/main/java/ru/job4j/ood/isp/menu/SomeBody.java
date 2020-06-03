10b3643ea872c081db55f229fe75db8de9b9b3e9
DenisViskov <DenisViskov16@gmail.com>
2020-06-03 23:26:42 +0300

    1. Хранилище продуктов [#259139]

---
+++ chapter_001 - TDD/src/main/java/ru/job4j/ood/isp/menu/SomeBody.java
@@ -0,0 +1,16 @@
+package ru.job4j.ood.isp.menu;
+
+/**
+ * Interface of somebody
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 03.06.2020
+ */
+public interface SomeBody {
+
+    /**
+     * Method should do something
+     */
+    void doSomething();
+}
---
+++ chapter_001 - TDD/src/main/java/ru/job4j/ood/isp/menu/Structure.java
@@ -0,0 +1,34 @@
+package ru.job4j.ood.isp.menu;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Interface of structure
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 03.06.2020
+ */
+public interface Structure<Something> {
+    /**
+     * Method should print whole menu
+     */
+    void printMenu();
+
+    /**
+     * Method should select
+     *
+     * @param name
+     * @return Something
+     */
+    Something select(String name);
+
+    /**
+     * Method should collects new Structure by given condition
+     *
+     * @param condition
+     * @param items
+     */
+    void collectStructure(Predicate<Something> condition, List<Something> items);
+}
