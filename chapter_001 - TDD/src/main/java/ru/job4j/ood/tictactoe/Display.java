0592e0bb67cd5da5a3361da0257b9e99649ff0ef
DenisViskov <DenisViskov16@gmail.com>
2020-06-11 17:04:54 +0300

    update

--- chapter_001 - TDD/src/main/java/ru/job4j/ood/tictactoe/Display.java
+++ chapter_001 - TDD/src/main/java/ru/job4j/ood/tictactoe/Display.java
@@ -12,21 +12,24 @@ import java.util.TreeMap;
 public class Display implements UI<BoxByThree> {
 
     private final Polygon polygon;
-    private final String emptyBox = " --- "
-            + System.lineSeparator()
-            + "|" + "   " + "|"
-            + System.lineSeparator()
-            + " --- ";
-    private final String zeroIntoBox = " --- "
-            + System.lineSeparator()
-            + "|" + " O " + "|"
-            + System.lineSeparator()
-            + " --- ";
-    private final String xIntoBox = " --- "
-            + System.lineSeparator()
-            + "|" + " X " + "|"
-            + System.lineSeparator()
-            + " --- ";
+    private final String emptyBox = " --- --- --- " + System.lineSeparator()
+            + "|" + "   " + "|" + "   " + "|" + "   " + "|" + System.lineSeparator()
+            + " ---" + " ---" + " ---" + System.lineSeparator()
+            + "|" + "   " + "|" + "   " + "|" + "   " + "|" + System.lineSeparator()
+            + " ---" + " ---" + " ---" + System.lineSeparator()
+            + "|" + "   " + "|" + "   " + "|" + "   " + "|" + System.lineSeparator()
+            + " ---" + " ---" + " ---" + System.lineSeparator();
+    private final Map<BoxByThree, Integer> coordinates = Map.of(BoxByThree.B1, 17,
+            BoxByThree.B2, 21,
+            BoxByThree.B3, 25,
+            BoxByThree.B4, 46,
+            BoxByThree.B5, 50,
+            BoxByThree.B6, 54,
+            BoxByThree.B7, 75,
+            BoxByThree.B8, 79,
+            BoxByThree.B9, 83);
+    private final char zeroIntoBox = 'O';
+    private final char xIntoBox = 'X';
 
     public Display(Polygon polygon) {
         this.polygon = polygon;
@@ -34,25 +37,16 @@ public class Display implements UI<BoxByThree> {
 
     @Override
     public void showPolygon() {
+        char[] points = emptyBox.toCharArray();
         Map<BoxByThree, Gamer> map = new TreeMap<>(getComparator());
         map.putAll(polygon.getCurrentMap());
-        StringBuilder builder = new StringBuilder();
-        int count = 1;
         for (Map.Entry<BoxByThree, Gamer> pair : map.entrySet()) {
-            if (count == 3) {
-                builder.append(System.lineSeparator());
-                count = 1;
-            }
-            if (pair.getValue() == null) {
-                builder.append(emptyBox);
-            }
             if (pair.getValue() != null) {
-                builder.append(pair.getValue().getFigure().equals(Figure.X)
-                        ? xIntoBox : zeroIntoBox);
+                points[coordinates.get(pair.getKey())] = pair.getValue().getFigure().equals(Figure.X)
+                        ? xIntoBox : zeroIntoBox;
             }
-            count++;
         }
-        System.out.println(builder);
+        System.out.print(points);
     }
 
     private Comparator<BoxByThree> getComparator() {
--- chapter_001 - TDD/src/test/java/ru/job4j/ood/tictactoe/DisplayTest.java
+++ chapter_001 - TDD/src/test/java/ru/job4j/ood/tictactoe/DisplayTest.java
@@ -1,11 +1,25 @@
 package ru.job4j.ood.tictactoe;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
 
 public class DisplayTest {
+
+    private final PrintStream console = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream byteArray = new PrintStream(out);
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(byteArray);
+    }
 
     @Test
     public void showPolygonTest() {
@@ -16,5 +30,17 @@ public class DisplayTest {
         place.addStep(BoxByThree.B5, second);
         Display display = new Display(place);
         display.showPolygon();
+        assertThat(out.toString(), is(" --- --- --- " + System.lineSeparator()
+                + "|" + " X " + "|" + "   " + "|" + "   " + "|" + System.lineSeparator()
+                + " ---" + " ---" + " ---" + System.lineSeparator()
+                + "|" + "   " + "|" + " O " + "|" + "   " + "|" + System.lineSeparator()
+                + " ---" + " ---" + " ---" + System.lineSeparator()
+                + "|" + "   " + "|" + "   " + "|" + "   " + "|" + System.lineSeparator()
+                + " ---" + " ---" + " ---" + System.lineSeparator()));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(console);
     }
 }
